a960a2b0d7b52c3637c8cbc5ceb5b860edddcda7
Gerardo Salinas <gerardosalinas96@hotmail.com>
2020-05-02 14:58:38 -0300

    commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const button1 = document.getElementById("btnSiguiente1")
+const textarea1 = document.getElementById("textAreaPedido");
+var checkbox1 = document.getElementById('checkbox1');
+var fechaHora = document.getElementById("fechaHora");
+
+var fecha = new Date().toISOString().split('T')[0];
+var hora = new Date().getHours().toString() + ":" + new Date().getMinutes().toString()
+console.log(fecha)
+console.log(hora)
+
+
+document.getElementById("date").setAttribute('min', fecha);
+document.getElementById("date").setAttribute('min', hora);
+
+
+checkbox1.addEventListener("change", validaCheckbox, false);
+function validaCheckbox() {
+    var checked = checkbox1.checked;
+    if (checked) {
+        ocultarFechaHora();
+    }
+    else {
+        mostrarFechaHora();
+    }
+}
+
+button1.addEventListener("click", siguiente1)
+
+function siguiente1() {
+
+    console.log("hola")
+
+}
+
+function ocultarFechaHora() {
+    fechaHora.style.display = "none"
+}
+function mostrarFechaHora() {
+    fechaHora.style.display = "block"
+}
