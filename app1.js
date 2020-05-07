e53d34b10085ee52112171bc9d761490973e7e97
Gerardo Salinas <gerardosalinas96@hotmail.com>
2020-05-07 12:04:56 -0300

    validando direcciones

--- app1.js
+++ app1.js
@@ -5,8 +5,17 @@ document.getElementById("btnManual").addEventListener("click", mostrarManual)
 function mostrarMapa() {
     document.getElementById("showMapa").style.display = "block"
     document.getElementById("showManual").style.display = "none"
+
+    document.getElementById("inputciudad").required = false;
+    document.getElementById("inputcalle").required = false;
+    document.getElementById("inputnumero").required = false;
+
 }
 function mostrarManual() {
     document.getElementById("showMapa").style.display = "none"
     document.getElementById("showManual").style.display = "block"
+
+    document.getElementById("inputciudad").required = true;
+    document.getElementById("inputcalle").required = true;
+    document.getElementById("inputnumero").required = true;
 }
